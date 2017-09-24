95516d309281b68c146b64b3157a359be3aa335c
delosiliana <dronixa@mail.ru>
2017-09-24 21:56:24 +0300

    Test main

--- main.rb
+++ main.rb
@@ -1,5 +1,15 @@
-require_relative 'dealer.rb.rb'
 require_relative 'player.rb'
+require_relative 'dealer.rb'
 require_relative 'deck.rb'
 require_relative 'card.rb'
 
+
+#loop do
+#  puts 'Введите Ваше имя:'
+#  name = gets.chomp
+#  puts 'Вы не ввели имя' if name.strip.empty?
+#end
+
+card = Card.new('K', '♥')
+p card
+p card.to_s
