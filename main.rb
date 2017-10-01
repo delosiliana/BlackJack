dd19a5981d60a490e12a7f63eeeea4cc9e54cfd0
delosiliana <dronixa@mail.ru>
2017-10-01 22:41:47 +0300

    main init

--- main.rb
+++ main.rb
@@ -2,7 +2,14 @@ require_relative 'hand.rb'
 require_relative 'deck.rb'
 require_relative 'card.rb'
 
+Class Main
+  attr_reader :deck, :hand, :card
 
+  def initialize
+    @hand = Hand.new
+    @deck = Deck.new
+  end
+  
 #loop do
 #  puts 'Введите Ваше имя:'
 #  name = gets.chomp
