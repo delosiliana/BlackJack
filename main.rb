fc60b4407fbc014636f106ab34879202e8aef515
delosiliana <dronixa@mail.ru>
2017-10-02 22:34:54 +0300

    rewrite mainv1

--- main.rb
+++ main.rb
@@ -17,11 +17,6 @@ end
 #  puts 'Вы не ввели имя' if name.strip.empty?
 #end
 
-card = Card.new('Q', '♣')
-p card
-p card.to_s
-#deck = Deck.new
-#p deck
 hand = Hand.new
 hand.deal
-hand.count_values
+p hand.count_values
