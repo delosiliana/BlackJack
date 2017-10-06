64878a039160b878a8db76ec94994c953062ae78
delosiliana <dronixa@mail.ru>
2017-10-06 22:38:08 +0300

    rewrite user

--- user.rb
+++ user.rb
@@ -1,12 +1,18 @@
 class User
-  attr_reader :hand
+
+  attr_accessor :hand, :cards, :coin
 
   def initialize(name)
     @name = name
     @coin = 100
-    @hand = Hand.new
+    @cards = Deck.new
+    @hand = []
   end
 
+  def deal
+    @hand << @cards.reach
+  end
+  
   def rate
     @coin -= 10
   end
@@ -19,11 +25,28 @@ class User
     @coin.zero?
   end
 
+  def show_card
+    @hand.each { |card| print "#{card.to_s} " }
+  end
+
+  def show_dealer
+    @hand.size { |card| print '*  ' }
+  end
+
   def reset_cards
     @cards.clear
   end
-  
+
   def limit_cards?
     @cards.size == 3
+  end
+  
+  def count_values
+    @count = 0
+    @hand.each do |card|
+      @count += card.value.to_i
+      @count += 10 if card.value == 1 && @count + 10 <= 21
+    end
+    @count
   end
 end
