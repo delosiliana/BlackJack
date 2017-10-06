db2607a1d167ce1e2bda751c3a135ecba151c200
delosiliana <dronixa@mail.ru>
2017-10-06 22:40:40 +0300

    rewrite main

--- main.rb
+++ main.rb
@@ -4,12 +4,11 @@ require_relative 'deck.rb'
 require_relative 'card.rb'
 
 class Main
-  attr_accessor :cards, :deck, :hand, :user
+  attr_accessor :cards, :deck, :hand, :user, :coin
 
   def initialize
     puts "Введите свое имя:"
-    name = gets.chomp.capitalize
-    puts "Здраствуйте #{name}"
+    name = gets.chomp.capitalize    
     @player = User.new(name)
     @dealer = User.new('Dealer')
     @bank = 0
@@ -18,14 +17,13 @@ class Main
   end
 
   def start_game
-    while @player.coin > 0
-      @player.reset_cards
-      @dealer.reset_cards
-      @deck.shuffle_cards
-      @player.deal
-      @dealer.deal
-      puts "**&***"
-    end
+    2.times{ add_card(@player) }
+    2.times{ add_card(@dealer) }
+    @player.rate
+    @dealer.rate
+    puts "Здраствуйте #{@name} у вас на руках карты #{@player.hand[0].rank}#{@player.hand[0].suit} #{@player.hand[1].rank}#{@player.hand[1].suit} у вас осталось #{@player.coin}$ сумма карт #{summa(@player)} "
+    puts "#{see_dealer} и осталось #{@dealer.coin}$"
+    menu
   end
 
   def menu
@@ -33,9 +31,9 @@ class Main
     puts "1- Взять одну карту  2- Пропустить ход  3- Открыться  "
     input = gets.to_i
     case input
-      when 1 then 
-      when 2 then 
-      when 3 then 
+      when 1 then player_give
+      when 2 then dealer_give
+      when 3 then show_card_player
       when 0 then exit
     end
   end
